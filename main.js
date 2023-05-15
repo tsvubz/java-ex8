771b5855c33cc1b1ce47005789ce039df839bf3f
Terence Svubure <tsvubure@gmail.com>
2023-05-15 13:46:57 +0200

    second

---
+++ main.js
@@ -0,0 +1,6 @@
+let personName = 'Terence Svubure';
+
+document.getElementById('demo').innerHTML = personName;
+
+let text = document.getElementById('demo').textContent;
+document.getElementById('demo2').innerHTML = text;
